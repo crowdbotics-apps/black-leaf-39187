43a48929e124c44a43ee429274898517bc5f69e9
Crowdbotics <team@crowdbotics.com>
2023-03-02 05:08:38 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "black-leaf-39187",
+  url: "https://black-leaf-39187.botics.co",
+  api: "https://black-leaf-39187.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
